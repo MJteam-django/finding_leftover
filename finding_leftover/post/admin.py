66a646ec4bed690ff35031e3b6b9a6a084394244
jeongseo21 <jeongseo21@gmail.com>
2020-10-24 14:54:54 +0900

    first commit

---
+++ finding_leftover/post/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import Post # 모델 만든거 가져오기
+
+admin.site.register(Post)
---
+++ finding_leftover/post/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    restaurant = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    #image = models.ImageField(default='media/default.jpg')
+    memo = models.TextField(blank=True)
+    local = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    soldout = models.BooleanField(default=False)
+    origin_price = models.IntegerField(default=0)
+    new_price = models.IntegerField(default=0)
+    poster = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
