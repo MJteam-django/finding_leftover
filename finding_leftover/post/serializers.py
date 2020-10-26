f5ee55824ac5f4d5f5c47a0b44587c1327440765
minseo <cms71198@gmail.com>
2020-10-26 18:50:56 +0900

    modelupdate

--- finding_leftover/post/serializers.py
+++ finding_leftover/post/serializers.py
@@ -5,8 +5,10 @@ from .models import Post, Store # Post 모델 가져오기
 
 # 모델을 가져와서 직렬화하겠다는거, 매개변수 모델 시리얼라이저
 class PostSerializer(serializers.ModelSerializer):
-    # 게시자는 변경할 수 없도록 
+    # 게시자는 변경할 수 없는 항목들
     poster = serializers.ReadOnlyField(source='poster.username')
+    restaurant = serializers.ReadOnlyField(source='poster.store.store_name')
+    local = serializers.ReadOnlyField(source='poster.store.store_adress')
 
     class Meta:
         model = Post
