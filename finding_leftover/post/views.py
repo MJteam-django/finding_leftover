3a91a69abd705fcf91f7618f3d5e4eb528fefb02
minseo <cms71198@gmail.com>
2020-10-27 00:31:14 +0900

    search

--- finding_leftover/post/views.py
+++ finding_leftover/post/views.py
@@ -1,8 +1,10 @@
 from rest_framework.response import Response
 from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.views import APIView
-from .models import Post
-from .serializers import PostSerializer
+from rest_framework.generics import ListAPIView
+from .models import Post, Store
+from .serializers import PostSerializer, StoreSerializer
+from rest_framework.filters import SearchFilter
 
 # 포스팅 목록 및 새 포스팅 작성
 class PostListAPIView(APIView):
@@ -20,3 +22,17 @@ class PostDetailAPIView(APIView):
     def get(self, request, pk):
         queryset = Post.objects.get(pk=pk)
         return Response({'post': queryset})
+
+
+class StoreListAPI(ListAPIView):
+    queryset = Store.objects.all()
+    serializer_class = StoreSerializer
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'store_list.html'
+
+    filter_backends = [SearchFilter]
+    search_fields = ['store_name']
+
+    def get(self, request):
+        queryset = Store.objects.all()
+        return Response({'stores': queryset})
